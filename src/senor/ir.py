6bce6cc71e8307aefcd69ad185343a325adf643b
jailbreak490 <jesperkehlet61@gmail.com>
2026-02-12 13:04:42 +0100

    fix in IR sensor

--- src/senor/ir.py
+++ src/senor/ir.py
@@ -3,14 +3,18 @@ import time
 
 sensor = ADC(Pin(26))   # <-- Correct class name
 
-while True:
-    value = sensor.read_u16()   # more accurate than .value()
-    print("Raw:", value)
+def ir():
+        value = sensor.read_u16()   # more accurate than .value()
+        print("Raw:", value)
 
-    if value < 20000:
-        print("BLACK LINE")
-    else:
-        print("WHITE SURFACE")
+        if value < 20000:
+            print("BLACK LINE")
+        else:
+            print("WHITE SURFACE")
 
-    print()
-    time.sleep(0.1)
+        print()
+        time.sleep(0.1)
+
+if __name__ == '__main__':
+    while True:
+        ir()
