cb356d8b51d5717962c462434f216e7619e57e24
jonastorp <jonastorp1@gmail.com>
2026-02-17 15:19:27 +0100

    tilføjet motor main til test og tilføjet en trim funktion til at kunne ændre styrken af en individuel motor

--- src/motor/main.py
+++ src/motor/main.py
@@ -1,8 +1,10 @@
 from machine import Pin, PWM
 from time import sleep
 
+
 class DCMotor:
-    def __init__(self, pin_forward, pin_backward, pin_enable, freq=1000):
+    def __init__(self, pin_forward, pin_backward, pin_enable, trim=1.0, freq=1000):
+        # Initialiser pins
         self.forward = PWM(Pin(pin_forward, Pin.OUT))
         self.backward = PWM(Pin(pin_backward, Pin.OUT))
         self.enable = PWM(Pin(pin_enable, Pin.OUT))
@@ -11,22 +13,18 @@ class DCMotor:
         self.backward.freq(freq)
         self.enable.freq(freq)
 
-        self.stop()
+        # Trim skal sættes FØR stop
+        self.trim = trim
 
-        #Enables the motor(turning on the power)
+        # Tænd motoren
         self.enable.duty_u16(65535)
 
-
-
+        # Stop motoren
+        self.stop()
 
     def set_speed(self, speed):
-        """
-        speed: -100 to 100
-        positive = forward
-        negative = backward
-        """
         speed = max(-100, min(100, speed))
-        duty = int(abs(speed) * 65535 / 100)
+        duty = int(abs(speed) * 65535 / 100 * self.trim)
 
         if speed > 0:
             self.backward.duty_u16(0)
@@ -41,19 +39,27 @@ class DCMotor:
         self.forward.duty_u16(0)
         self.backward.duty_u16(0)
 
-left_motor = DCMotor(3, 4, 5)
-right_motor = DCMotor(1, 2, 0)
+
+# Opret motorer med ;pin_forward, pin_backwards, pin_enable, trim(styke %)
+left_motor = DCMotor(3, 4, 5, trim=0.75)
+right_motor = DCMotor(1, 2, 0, trim=1.0)
+
 
 def main():
-    left_motor.set_speed(40)
-    right_motor.set_speed(-50)
-    sleep(2)
-    left_motor.stop()
-    right_motor.stop()
-    sleep(0.1)
-    left_motor.set_speed(-40)
+    # Frem
+    left_motor.set_speed(50)
     right_motor.set_speed(50)
     sleep(2)
+
+    left_motor.stop()
+    right_motor.stop()
+    sleep(1)
+
+    # Bak
+    left_motor.set_speed(-50)
+    right_motor.set_speed(-50)
+    sleep(2)
+
     left_motor.stop()
     right_motor.stop()
 
