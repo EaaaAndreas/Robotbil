a3101638fd352d4fbc99953b21047a43ff494753
andre <eaa25ansn@students.eaaa.dk>
2026-02-23 00:18:22 +0100

    Refactored the porting. It should be just about done. Just need some fine-tuning and adding callbacks

--- src/main.py
+++ src/main.py
@@ -1,21 +1,39 @@
 # /main.py
-import socket
-import machine
-import ustruct
+from connectivity.porting import *
 
 
+PRG_NONE = 0
+PRG_FOOTBALL = 1
+PRG_WALL_FOLLOW = 2
+PRG_SUMO = 3
+# If adding programs, remember to update `select_program`
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+CURRENT_PROGRAM = PRG_NONE
+
+def set_program(prg):
+    global CURRENT_PROGRAM
+    if prg in (PRG_NONE, PRG_FOOTBALL, PRG_WALL_FOLLOW, PRG_SUMO):
+        CURRENT_PROGRAM = prg
+    else:
+        CURRENT_PROGRAM = PRG_NONE
+    return CURRENT_PROGRAM
+
+def ping(*_): #Ping
+    return CURRENT_PROGRAM #TODO: Add battery/others
+
+
+add_callback(ping)
+add_callback(set_program)
+
+# TODO: Add battery callback
+# TODO: Add football callback
+
+
 try:
-    sock.bind(('0.0.0.0', 50001))
-
     while True:
-        data, addr = sock.recvfrom(1024)
-        print(data[0], data[1:], addr)
-        if data[1:] == b'DISCOVER':
-            sock.sendto(bytes([data[0]]) + b'OFFER', addr)
-        elif data[1:] == b'REQUEST':
-            sock.sendto(bytes([data[0]]) + b'ACCEPT', addr)
-
+        udp_task()
 finally:
-    sock.close()
+    close_socket()
+
+
